746d9887ee2bf9cca93b5eade3f540f5ed838231
Sal Savage <salsavage@google.com>
2022-10-06 14:42:41 +0000

    RMX2061: Enable bluetooth profiles with system_ext.prop
    Signed-off-by: debdeep199x <debdeep199x@gmail.com>
    Signed-off-by: pri0818 <priyanshusinghal0818@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -182,18 +182,11 @@ TARGET_BOARD_PLATFORM := atoll
 TARGET_BOOTLOADER_BOARD_NAME := atoll
 
 # Properties
-<<<<<<< HEAD:BoardConfigCommon.mk
-TARGET_ODM_PROP += $(COMMON_PATH)/odm.prop
-TARGET_PRODUCT_PROP += $(COMMON_PATH)/product.prop
-TARGET_SYSTEM_PROP += $(COMMON_PATH)/system.prop
-TARGET_VENDOR_PROP += $(COMMON_PATH)/vendor.prop
-=======
 TARGET_ODM_PROP += $(DEVICE_PATH)/odm.prop
 TARGET_PRODUCT_PROP += $(DEVICE_PATH)/product.prop
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 TARGET_SYSTEM_EXT_PROP += $(DEVICE_PATH)/system_ext.prop
->>>>>>> 072c337 (RMX2061: Merge RMX2061 device tree into common tree):BoardConfig.mk
 
 # QCOM
 BOARD_USES_QCOM_HARDWARE := true
